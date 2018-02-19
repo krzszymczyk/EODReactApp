65c163ae8137b56822285dfeb897fc41a248e65a
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-02-19 16:23:31 +0100

    Creating new project
    New project template

---
+++ EODReactApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EODReactApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
